de0e0d550a5a3a9fc8992bc1bbbe6c821d3088f4
bwebb29 <webb96bee@gmail.com>
2025-05-20 14:29:05 -0700

    app conversion

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-app.js"
+
+const firebaseConfig = {}
+
+const app = initializeApp(firebaseConfig)
+
+console.log(app)
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
